8cc723977ace1640373c6a8af6435421ba6147c5
daimin <1580426939@qq.com>
2019-04-17 15:07:23 +0800

    InetAddress操作域名和IP

---
+++ java/day11_web/Address/InetAddressTest.java
@@ -0,0 +1,20 @@
+package day11_web.Address;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @Description:
+ * @author: daimin
+ * @date: Create in 14:51 2019/4/17
+ */
+public class InetAddressTest {
+    public static void main(String[] args) {
+        try {
+            InetAddress a0 = InetAddress.getByName("www.baidu.com");
+            System.out.println("Address = " + a0.getAddress() + "\nHostName(域名) = " + a0.getHostName() + "\nHostAddress(IP) = " + a0.getHostAddress());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ java/day11_web/Address/InteAddress.md
@@ -0,0 +1,3 @@
+## IP vs 域名
+> Internet 上的主机有两种方式表示地址，分别为域名和 IP 地址。
+java.net 包中的 InetAddress 类对象包含一个 Internet 主机地址的域名和 IP 地址
